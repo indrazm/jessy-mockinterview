8a2192f8d13ae20bb7ba4cb3628c5c2ca5a035a9
Indra Zulfi <me@indrazm.com>
2024-06-15 11:04:58 +0700

    layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,10 @@
 import { Header } from "./header";
-import { Uploader } from "./uploader";
 
 export const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main className="max-w-4xl my-12 m-auto">{children}</main>
-      <Uploader />
     </>
   );
 };
